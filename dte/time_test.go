72110684b602b9bd24d0573382fd2d085e577570
Peter <peterHoburg@users.noreply.github.com>
2024-12-24 15:01:09 -0500

    Added with space test

--- dte/time_test.go
+++ dte/time_test.go
@@ -102,6 +102,12 @@ func TestParse(t *testing.T) {
 			wantError: false,
 		},
 		{
+			name:      "valid time with space -5",
+			inputTime: "10:04:05 -05:00",
+			want:      `"15:04:05Z"`,
+			wantError: false,
+		},
+		{
 			name:      "valid time +5",
 			inputTime: "20:04:05+05:00",
 			want:      `"15:04:05Z"`,
