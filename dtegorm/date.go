66db22e38381c264287f63d5f911c0bca23703c2
Peter <peterHoburg@users.noreply.github.com>
2024-12-28 21:59:31 -0500

    Moved test funcs added date

---
+++ dtegorm/date.go
@@ -0,0 +1,84 @@
+package dtegorm
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/peterHoburg/go-date-and-time-extension/dte"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+var (
+	ErrNewDate             = errors.New("failed to create new date")
+	ErrDateScan            = errors.New("failed to scan value into date struct")
+	ErrDateScanInvalidType = errors.New("invalid type passed to scan")
+)
+
+type Date struct { //nolint:recvcheck
+	dte.Date
+}
+
+func NewDate(s string) (Date, error) {
+	timeInstance := Date{}
+
+	err := timeInstance.SetFromString(s)
+	if err != nil {
+		return Date{}, fmt.Errorf("%w: %w", ErrNewDate, err)
+	}
+
+	return timeInstance, nil
+}
+
+// GormDataType returns gorm common data type. This type is used for the field's column type.
+func (Date) GormDataType() string {
+	return "date"
+}
+
+// GormDBDataType returns gorm DB data type based on the current using database.
+func (Date) GormDBDataType(db *gorm.DB, _ *schema.Field) string {
+	switch db.Dialector.Name() {
+	case "mysql":
+		return "DATE"
+	case "postgres":
+		return "DATE"
+	case "sqlserver":
+		return "DATE"
+	case "sqlite":
+		return "DATE"
+	default:
+		return ""
+	}
+}
+
+// Scan implements sql.Scanner interface and scans value into Date,.
+func (d *Date) Scan(src interface{}) error {
+	switch v := src.(type) {
+	case []byte:
+		err := d.SetFromString(string(v))
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrDateScan, err)
+		}
+	case string:
+		err := d.SetFromString(v)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrDateScan, err)
+		}
+	case time.Time:
+		err := d.SetFromTime(v)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrDateScan, err)
+		}
+	default:
+		return ErrDateScanInvalidType
+	}
+
+	return nil
+}
+
+// Value implements driver.Valuer interface and returns string format of Date.
+func (d Date) Value() (driver.Value, error) {
+	return d.String(), nil
+}
--- dtegorm/time_test.go
+++ dtegorm/time_test.go
@@ -2,109 +2,15 @@ package dtegorm_test
 
 import (
 	"fmt"
-	"log"
 	"strings"
 	"testing"
-	"time"
-
-	"github.com/google/uuid"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 
 	"github.com/peterHoburg/go-date-and-time-extension/dtegorm"
 )
 
-type Example struct {
+type TimeExample struct {
 	ID       uint `gorm:"primarykey"`
 	OnlyTime dtegorm.Time
-}
-
-func Open(dsn string, db *gorm.DB) *gorm.DB {
-	if db != nil {
-		openDB, err := db.DB()
-		if err != nil {
-			log.Fatal("Error getting DB")
-		}
-
-		err = openDB.Close()
-		if err != nil {
-			log.Fatal("Error closing DB")
-		}
-	}
-
-	var err error
-
-	var localDB *gorm.DB
-
-	retries := 3
-
-	retry := 0
-
-	for retry < retries {
-		localDB, err = gorm.Open(postgres.Open(dsn))
-		if err == nil {
-			break
-		}
-
-		retry++
-
-		time.Sleep(3 * time.Second)
-	}
-
-	if err != nil {
-		log.Fatal("Error connecting to database")
-	}
-
-	if localDB == nil {
-		log.Fatal("Error connecting to database")
-	}
-
-	db = localDB.Debug()
-
-	return db
-}
-
-func CreateDB(dbName string, db *gorm.DB) {
-	result := db.Exec("CREATE DATABASE " + dbName)
-	if result.Error != nil {
-		log.Fatal("Error creating database")
-	}
-}
-
-func DeleteDB(dbName string, db *gorm.DB) {
-	result := db.Exec("DROP DATABASE " + dbName)
-	if result.Error != nil {
-		log.Fatal("Error deleting database")
-	}
-}
-
-func RunMigrations(db *gorm.DB) {
-	err := db.AutoMigrate(&Example{})
-	if err != nil {
-		log.Fatal("Error migrating database")
-	}
-}
-
-func Setup() (string, string, *gorm.DB) {
-	const DSN = "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=GMT"
-
-	var db *gorm.DB
-
-	db = Open(DSN, db)
-
-	dbName := strings.ReplaceAll("testdb-"+uuid.New().String(), "-", "")
-
-	CreateDB(dbName, db)
-	dsn := strings.ReplaceAll(DSN, "dbname=postgres", "dbname="+dbName)
-	db = Open(dsn, db)
-	RunMigrations(db)
-
-	return dbName, dsn, db
-}
-
-func Teardown(dbName string, dsn string, db *gorm.DB) {
-	db = Open(dsn, db)
-	DeleteDB(dbName, db)
 }
 
 func ExampleTime() {
@@ -113,24 +19,19 @@ func ExampleTime() {
 	defer Teardown(dbName, dsn, db)
 	// ^^^ Setup for the PG DB. This can be ignored
 
-	type Example struct {
-		ID       uint `gorm:"primarykey"`
-		OnlyTime dtegorm.Time
-	}
-
 	onlyTime, err := dtegorm.NewTime("10:04:05-05:00")
 	if err != nil {
 		return
 	}
 
-	example := Example{OnlyTime: onlyTime}
+	example := TimeExample{OnlyTime: onlyTime}
 
 	createResult := db.Create(&example)
 	if createResult.Error != nil {
 		return
 	}
 
-	var exampleResult Example
+	var exampleResult TimeExample
 
 	getResult := db.First(&exampleResult, example.ID)
 	if getResult.Error != nil {
@@ -159,7 +60,7 @@ func TestTime(t *testing.T) {
 	db.Raw(
 		"SELECT column_name, data_type " +
 			"FROM information_schema.columns " +
-			"WHERE table_name = 'examples' AND column_name = 'only_time'",
+			"WHERE table_name = 'time_examples' AND column_name = 'only_time'",
 	).Scan(&result)
 
 	if result.ColumnName != "only_time" && result.DataType != "time with time zone" {
@@ -171,14 +72,14 @@ func TestTime(t *testing.T) {
 		t.Errorf("Error creating time")
 	}
 
-	example := Example{OnlyTime: onlyTime}
+	example := TimeExample{OnlyTime: onlyTime}
 
 	dbResult := db.Create(&example)
 	if dbResult.Error != nil {
 		t.Errorf("Error creating example")
 	}
 
-	var exampleResult Example
+	var exampleResult TimeExample
 
 	dbResult = db.First(&exampleResult, example.ID)
 	if dbResult.Error != nil {
